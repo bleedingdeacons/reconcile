cd40a2314541fa63ce9d86b151803ee75f9bc925
Dave <crazeydave@users.noreply.github.com>
2026-07-20 17:51:42 +0100

    Release 1.13.20

--- reconcile.php
+++ reconcile.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * Plugin Name: Reconcile
  * Description: Import/Export of member, group and position data from spreadsheets using Unity framework.
- * Version: 1.13.19
+ * Version: 1.13.20
  * Requires at least: 6.0
  * Requires PHP: 8.1
  * Requires Plugins: unity, scrutiny
